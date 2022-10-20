12c95677a381bec1e3aefb62ed4059d4ba69e938
Mateus C <mts.c@live.com>
2022-10-20 12:01:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,28 @@
 import cache
 import matplotlib.pyplot as plt
 
-def main():
+#cache_simulator <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada
+    #cacheSize = 64 # in KB 1= 1KB, 64 = 64KB
+    #byteAddress = 8 # 8 bits = 1 byte
+    #blockSize =  32 # 8 bits = 1 byte
+    #memoryAddressSize = 32 # 32 or 64 bits
+    #associativeWays = 1 # 1 = Direct Mapping, 2 = 2-way associative, 4 = 4-way associative, 8 = 8-way associative, 16 = 16-way associative
+#nsets, blockSize, associativity, replacementPolicy, outputFlag, inputFile
 
-    cacheSize = 64# Convert to first argument into KB, 32 = 32KB, 64 = 64KB, 128 = 128KB, 256 = 256KB
-    byteAddress = 8 # 8 bits = 1 byte
-    blockSize =  32 # 8 bits = 1 byte
-    memoryAddressSize = 32 # 32 or 64 bits
-    associativeWays = 1 # 1 = Direct Mapping, 2 = 2-way associative, 4 = 4-way associative, 8 = 8-way associative, 16 = 16-way associative
-    accessNumber = 1000000 # Number of accesses for simulation
+def main(nsets, blockSize, associativity, replacementPolicy, outputFlag, inputFile):
+    byteAddress = 8 
+    memoryAcess = []
+    with open(f'{inputFile}.txt', 'r') as txt:
+        for line in txt:
+            memoryAcess.append(int(line))
 
-    DirectMap = cache.CacheConfig(32, byteAddress, blockSize, memoryAddressSize, associativeWays)
-    hitMap32 = cache.cacheDirectMapAccess(DirectMap, accessNumber)
-
-    DirectMap = cache.CacheConfig(64, byteAddress, blockSize, memoryAddressSize, associativeWays)
-    hitMap64 = cache.cacheDirectMapAccess(DirectMap, accessNumber)
-
-    DirectMap = cache.CacheConfig(128, byteAddress, blockSize, memoryAddressSize, associativeWays)
-    hitMap128 = cache.cacheDirectMapAccess(DirectMap, accessNumber)
-
-    #AssociativeMap16way = cache.CacheConfig(32, byteAddress, blockSize, memoryAddressSize, 16)
-    #hitAw16 = cache.cacheAssociativeMapAccess(AssociativeMap16way, accessNumber, 16)
-
-    AssociativeMap16way = cache.CacheConfig(32, byteAddress, blockSize, memoryAddressSize, 16)
-    hitAw1632 = cache.cacheAssociativeMapAccess(AssociativeMap16way, accessNumber, 16)
-
-    AssociativeMap16way = cache.CacheConfig(64, byteAddress, blockSize, memoryAddressSize, 16)
-    hitAw1664 = cache.cacheAssociativeMapAccess(AssociativeMap16way, accessNumber, 16)
-
-    AssociativeMap16way = cache.CacheConfig(128, byteAddress, blockSize, memoryAddressSize, 16)
-    hitAw16128 = cache.cacheAssociativeMapAccess(AssociativeMap16way, accessNumber, 16)
-
-    #AssociativeMap16way = cache.CacheConfig(cacheSize, byteAddress, blockSize, memoryAddressSize, 8)
-    #hitAw8 = cache.cacheAssociativeMapAccess(AssociativeMap16way, accessNumber, 8)
-
-    #AssociativeMap16way = cache.CacheConfig(cacheSize, byteAddress, blockSize, memoryAddressSize, 4)
-    #hitAw4 = cache.cacheAssociativeMapAccess(AssociativeMap16way, accessNumber, 4)
-
-    #plot a graph hit rate vs cache size
-    plt.plot([32 ,64, 128], [hitMap32, hitMap64, hitMap128], label = 'Direct Map')
-    plt.plot([32 ,64, 128], [hitAw1632, hitAw1664, hitAw16128], label = 'Associative 16')
-
-    plt.xlabel('Cache Size (KB)')
-    plt.ylabel('Hit Rate (%)')
-    plt.show()
+    if associativity == 1:
+            cache.cacheDirectMapAccess(cache.CacheConfig(nsets, blockSize, associativity, byteAddress), memoryAcess)
+    elif associativity > 1:
+            cache.cacheAssociativeMapAccess(cache.CacheConfig(nsets, blockSize, associativity, byteAddress), memoryAcess)
+    else:
+        print("Invalid Associative-Way")
+        exit()
 
 if __name__ == "__main__":
     main()
