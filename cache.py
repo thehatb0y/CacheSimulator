5a8861c49d78330b0a811a008c26fe55a8281e19
Mateus C <mts.c@live.com>
2022-10-31 06:26:23 -0300

    Update cache.py

--- cache.py
+++ cache.py
@@ -15,8 +15,8 @@ class CacheConfig():
         self.index = int(math.log(self.cacheLines,2)) # number of bits for index
         self.des = int(math.log(self.blockSize/self.byteAddress,2)) # number of bits for des
         self.trueCacheSize = round(((self.cacheLines * self.associativeWays) * (self.blockSize + (self.memoryAddressSize - self.index - self.des)+1))/8192, 2) # Conversion for true cache size in KB 
-        
-        print(f'\nIndex: {self.index}b\t Tag:{self.memoryAddressSize-self.index-self.des}b\t Des:{self.des}b\t CacheLines:{self.cacheLines}\t AssociativeWays:{self.associativeWays}')
+        print("\n[Cache Configuration]")
+        print(f'Index: {self.index}b\t Tag:{self.memoryAddressSize-self.index-self.des}b\t Des:{self.des}b\t CacheLines:{self.cacheLines}\t AssociativeWays:{self.associativeWays}')
         print(f'BlockSize:{int(self.blockSize/8)}B\t ByteAddress: {int(self.byteAddress/8)}B\t MemoryAddressSize: {self.memoryAddressSize}b\t ')
         print(f'CacheSize:{int(self.cacheSize/1024)}KB\t TrueCacheSize: {self.trueCacheSize}KB\t TrueCacheSize is {100 * float(self.trueCacheSize)/(self.cacheSize/1024)- 100}% bigger')
 
@@ -32,7 +32,8 @@ def checkCache(cacheMemory):
             return False
     return True
 
-def cacheDirectMapAccess(DirectMap, memoryAcess):
+def cacheDirectMapAccess(DirectMap, memoryAcess, outputFlag):
+
     cl = DirectMap.cacheLines
     cm = startCache(cl)
     des = DirectMap.des
@@ -43,36 +44,37 @@ def cacheDirectMapAccess(DirectMap, memoryAcess):
     compulsoryMiss = 0
     capacityMiss = 0
     conflictMiss = 0
-
+    
     for binary in memoryAcess:
-        #print acessNumber as decimal
-        address = int(binary, 2)
+        if des == 0:
+            cmIndex = int(binary[32-index:],2)
+        else:
+            cmIndex = int(binary[32-des-index:-des],2)
 
-        if  cm[address%cl]['val'] == 1:
-            if  cm[address%cl]['tag'] == binary[:-des-index]:
+        if  cm[cmIndex]['val'] == 1:
+            if  cm[cmIndex]['tag'] == binary[:-des-index]:
                 hit = hit + 1
             else:
                 miss = miss +1
                 conflictMiss = conflictMiss + 1
-                cm[address%cl]['tag'] = binary[:-des-index]
-                cm[address%cl]['val'] = 1
+                cm[cmIndex]['tag'] = binary[:-des-index]
+                cm[cmIndex]['val'] = 1
                 if checkCache(cm) == False:
                     capacityMiss = capacityMiss + 1
         else:
             miss = miss + 1
             compulsoryMiss = compulsoryMiss + 1
-            cm[address%cl]['tag'] = binary[:-des-index]
-            cm[address%cl]['val'] = 1
+            cm[cmIndex]['tag'] = binary[:-des-index]
+            cm[cmIndex]['val'] = 1
+    print("\n[Cache Simulation]")   
+    if int(outputFlag) == 0:
+        print(f'Hit:{hit}\t Miss:{miss}\t HitRate:{round(hit/len(memoryAcess)*100, 3)}%\t TotalAccess:{hit+miss}')
+        print(f'MissCompulsory:{compulsoryMiss}\t MissConflict:{conflictMiss}\t MissCapacity:{capacityMiss}\t')
+    else:
+        print(f'{hit+miss}, {round(hit/len(memoryAcess)*100, 3)}, {round(miss/len(memoryAcess)*100, 3)}, {round(compulsoryMiss/len(memoryAcess)*100, 3)}, {round(capacityMiss/len(memoryAcess)*100, 3)}, {round(conflictMiss/len(memoryAcess)*100, 3)}')
+        return f'{hit+miss}, {round(hit/len(memoryAcess)*100, 3)}, {round(miss/len(memoryAcess)*100, 3)}, {round(compulsoryMiss/len(memoryAcess)*100, 3)}, {round(capacityMiss/len(memoryAcess)*100, 3)}, {round(conflictMiss/len(memoryAcess)*100, 3)}'
 
-    #Total de acessos, Taxa de hit, Taxa de miss, Taxa de miss compulsório, Taxa de miss de capacidade, Taxa de miss de conflito 
-    print(f'Hit:{hit}\t Miss:{miss}\t HitRate:{round(hit/len(memoryAcess)*100, 3)}%\t TotalAccess:{hit+miss}')
-    print(f'MissCompulsory:{compulsoryMiss}\t MissConflict:{conflictMiss}\t MissCapacity:{capacityMiss}\t')
-    
-    return round(hit/len(memoryAcess)*100, 3)
-
-
-
-def cacheAssociativeMapAccess(DirectMap, memoryAcess):
+def cacheAssociativeMapAccess(DirectMap, memoryAcess, outputFlag):
     cl = DirectMap.cacheLines
     des = DirectMap.des
     index = DirectMap.index
@@ -84,17 +86,18 @@ def cacheAssociativeMapAccess(DirectMap, memoryAcess):
     capacityMiss = 0
     conflictMiss = 0
 
-    #create a list of cacheMemory, the list have size of associativeWays and each item from the list should call startCache function
     cm = [startCache(cl) for i in range(DirectMap.associativeWays)]
-
-    #check check if tag = 0 in each cm list
+    
     for binary in memoryAcess:
+        if des == 0:
+            cmIndex = int(binary[32-index:],2)
+        else:
+            cmIndex = int(binary[32-des-index:-des],2)
 
-        address = int(binary, 2)
         isTagTrue = False
         for j in range(DirectMap.associativeWays):
-            if  cm[j][address%cl]['val'] == 1:
-                if  cm[j][address%cl]['tag'] == binary[:-des-index]:
+            if  cm[j][cmIndex]['val'] == 1:
+                if  cm[j][cmIndex]['tag'] == binary[:-des-index]:
                     hit = hit + 1
                     isTagTrue = True
                     break
@@ -104,23 +107,26 @@ def cacheAssociativeMapAccess(DirectMap, memoryAcess):
             compulsoryMiss = compulsoryMiss + 1
             isPositionFree = False
             for j in range(DirectMap.associativeWays):
-                if  cm[j][address%cl]['val'] == 0:
-                    cm[j][address%cl]['tag'] = binary[:-des-index]
-                    cm[j][address%cl]['val'] = 1
-                    cm[j][address%cl]['data'] = binary
+                if  cm[j][cmIndex]['val'] == 0:
+                    cm[j][cmIndex]['tag'] = binary[:-des-index]
+                    cm[j][cmIndex]['val'] = 1
+                    cm[j][cmIndex]['data'] = binary
                     isPositionFree = True
                     break
             
             if isPositionFree == False:
                 conflictMiss = conflictMiss + 1
-                if checkCache(cm) == False:
-                    capacityMiss = capacityMiss + 1
                 addRandom = int(random.randrange(0, DirectMap.associativeWays))
-                cm[addRandom][address%cl]['tag'] = binary[:-des-index]
-                cm[addRandom][address%cl]['val'] = 1
-                cm[addRandom][address%cl]['data'] = binary
-            
-    print(f'Hit:{hit}\t Miss:{miss}\t HitRate:{round(hit/len(memoryAcess)*100, 3)}%\t TotalAccess:{hit+miss}')
-    print(f'MissCompulsory:{compulsoryMiss}\t MissConflict:{conflictMiss}\t MissCapacity:{capacityMiss}')
+                if checkCache(cm[addRandom]) == False:
+                    capacityMiss = capacityMiss + 1
+                cm[addRandom][cmIndex]['tag'] = binary[:-des-index]
+                cm[addRandom][cmIndex]['val'] = 1
+                cm[addRandom][cmIndex]['data'] = binary
 
-    return round(hit/len(memoryAcess)*100, 3)
+    print("\n[Cache Simulation]")        
+    if int(outputFlag) == 0:
+        print(f'Hit:{hit}\t Miss:{miss}\t HitRate:{round(hit/len(memoryAcess)*100, 3)}%\t TotalAccess:{hit+miss}')
+        print(f'MissCompulsory:{compulsoryMiss}\t MissConflict:{conflictMiss}\t MissCapacity:{capacityMiss}\t')
+    else:
+        print(f'{hit+miss}, {round(hit/len(memoryAcess)*100, 3)}, {round(miss/len(memoryAcess)*100, 3)}, {round(compulsoryMiss/len(memoryAcess)*100, 3)}, {round(capacityMiss/len(memoryAcess)*100, 3)}, {round(conflictMiss/len(memoryAcess)*100, 3)}')
+        return f'{hit+miss}, {round(hit/len(memoryAcess)*100, 3)}, {round(miss/len(memoryAcess)*100, 3)}, {round(compulsoryMiss/len(memoryAcess)*100, 3)}, {round(capacityMiss/len(memoryAcess)*100, 3)}, {round(conflictMiss/len(memoryAcess)*100, 3)}'
